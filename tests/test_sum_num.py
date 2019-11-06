d84daec552786d3d706a13b9f864ee1029586117
Richie <richieganney@gmail.com>
2019-11-06 11:46:33 +0000

    refactor

--- tests/test_sum_num.py
+++ tests/test_sum_num.py
@@ -10,41 +10,23 @@ class TestStringMethods(unittest.TestCase):
     
     def test_sum(self):
 
-        # first store the expected result in a variable
         result_add = sum(3, 4)
-
-        # check if the result is equal to expected result
-        # here the result should be equal to 7.
         self.assertEquals(7, result_add)
 
     def test_subtract(self):
 
-        # first store the expected result in a variable
         result_subtract = subtract(4, 3)
-
-        # check if the result is equal to expected result
-        # here the result should be equal to 7.
         self.assertEquals(1, result_subtract)
 
     def test_divide(self):
 
-        # first store the expected result in a variable
         result_divide = divide(10, 2)
-
-        # check if the result is equal to expected result
-        # here the result should be equal to 7.
         self.assertEquals(5, result_divide)
 
     def test_multiply(self):
 
-        # first store the expected result in a variable
         result_multiply = multiply(10, 2)
-
-        # check if the result is equal to expected result
-        # here the result should be equal to 7.
         self.assertEquals(20, result_multiply)
-
-        
 
 if __name__ == '__main__':
     unittest.main()
